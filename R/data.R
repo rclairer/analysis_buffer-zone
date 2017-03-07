76b419106db90d8c2931a4edce669aefbc52fa89
rclairer <clairerosemond@gmail.com>
2017-03-07 12:49:48 -0500

    for steve

--- R/data.R
+++ R/data.R
@@ -236,11 +236,11 @@ transform_data <- function(data, transform_type = c("square_root", "cube_root", 
   if (transform_type == "square_root_plus_number")
     data <- sqrt(data)+ 0.3 # 0.11
   if (transform_type == "box-cox")
-    data <- sqrt(data + 0.1) # c = 0.1, lambda = 0.5
+   data <- sqrt(data + 0.1) # c = 0.1, lambda = 0.5
   #if (transform_type == "box-cox")
    # data <- sqrt(data + 1)
   #if (transform_type == "box-cox")
-    #data <- (data + 1)^(1/4) # c = 0.1, lambda = 0.25
+   # data <- (data + 1)^(1/4) # c = 1, lambda = 0.25
   if (transform_type == "log + 1")
     data <- log(1 + data)
   data
